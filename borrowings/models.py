7404fad5d88e0791108c6e88ee4be5a1ee089bba
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-04 15:38:50 +0100

    Add is_active field to Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -16,6 +16,7 @@ class Borrowing(models.Model):
     user = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name="borrowings"
     )
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.book} - {self.expected_return_date}"
