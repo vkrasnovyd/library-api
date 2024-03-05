4d6f05507447c4eec8b73c0815247724d58e3af0
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-05 18:53:57 +0100

    Add IsAdminUserOrReadOnly permission

--- library_api/permissions.py
+++ library_api/permissions.py
@@ -30,3 +30,15 @@ class IsUserAdminOrOwnUserProfileAccessOnly(BasePermission):
                 and request.user.is_staff
             )
         )
+
+
+class IsAdminUserOrReadOnly(BasePermission):
+    """
+    The request is authenticated as an admin, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS
+            or (request.user and request.user.is_staff)
+        )
