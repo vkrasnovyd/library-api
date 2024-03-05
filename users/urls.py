48fbb34087ac17f9d683252ab29e1c3131b5ec6a
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-05 18:09:44 +0100

    Add UserViewSet to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from users.views import CreateUserView, CreateTokenView
+from users.views import CreateUserView, CreateTokenView, UserViewSet
+
+router = routers.DefaultRouter()
+router.register("users", UserViewSet, basename="user")
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("login/", CreateTokenView.as_view(), name="login"),
+    path("", include(router.urls)),
 ]
 
 
