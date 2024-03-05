35773e6ef7e3366bcd62ba71dbd2d41fc533bb20
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-05 18:09:17 +0100

    Add UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, viewsets, mixins
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+from paginators import Pagination
+from permissions import IsUserAdminOrOwnUserProfileAccessOnly
 from users.serializers import UserSerializer
 
 
@@ -11,3 +14,15 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    permission_classes = (IsUserAdminOrOwnUserProfileAccessOnly,)
+    pagination_class = Pagination
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
