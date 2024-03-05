7fcd2cfbed0c2b8475f33b1aacf78cb64c425289
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-05 18:54:52 +0100

    Add BookViewSet

---
+++ books/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from books.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = (
+            "title",
+            "author",
+            "cover",
+            "total_amount",
+            "inventory",
+            "daily_fee",
+        )
+
+
+class BookListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ("title", "author", "inventory", "daily_fee")
--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookListSerializer, BookSerializer
+from paginators import Pagination
+from permissions import IsAdminUserOrReadOnly
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUserOrReadOnly,)
+    pagination_class = Pagination
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return BookListSerializer
+
+        return BookSerializer
