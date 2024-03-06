ddf999897126ce9e01f8ac0112daaddb86d39c42
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-06 23:29:34 +0100

    Add get_full_absolute_url() method to all models

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 
+from settings import BASE_URL
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, blank=True)
@@ -11,3 +13,6 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:user-detail", kwargs={"pk": self.id})
+
+    def get_full_absolute_url(self):
+        return f"{BASE_URL}{self.get_absolute_url()}"
