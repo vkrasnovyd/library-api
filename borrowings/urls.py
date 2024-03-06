77bb9c26cddfd91e9d4fc358da5b8c63a18efdd3
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-06 12:23:43 +0100

    Add BorrowingViewSet to urlpatterns

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,4 +1,12 @@
-urlpatterns = []
+from rest_framework import routers
+
+from borrowings.views import BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingViewSet, basename="borrowing")
+
+
+urlpatterns = router.urls
 
 
 app_name = "borrowings"
