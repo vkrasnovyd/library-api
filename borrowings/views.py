e613746c4c895ddf6cc578c8c1a44fe1df146c42
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-06 12:37:01 +0100

    Add filtering borrowings by user_id for staff

--- borrowings/views.py
+++ borrowings/views.py
@@ -22,4 +22,11 @@ class BorrowingViewSet(
         if not self.request.user.is_staff:
             queryset = queryset.filter(user=self.request.user)
 
+        else:
+            if self.action == "list":
+                user_id = self.request.query_params.get("user_id", None)
+
+                if user_id:
+                    queryset = queryset.filter(user_id=user_id)
+
         return queryset
