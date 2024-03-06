2c8a9125d80c62cba44b8426c8e9355c79c3152e
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-07 00:38:32 +0100

    Refactor Borrowing.borrow_date

--- borrowings/models.py
+++ borrowings/models.py
@@ -3,13 +3,14 @@ import datetime
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+from django.utils.timezone import now
 
 from books.models import Book
 from settings import BASE_URL
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
+    borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
@@ -24,6 +25,8 @@ class Borrowing(models.Model):
         return f"{self.book} - {self.expected_return_date}"
 
     def save(self, *args, **kwargs):
+        if not self.id:
+            self.borrow_date = now().date()
         self.expected_return_date = self.borrow_date + datetime.timedelta(
             weeks=2
         )
