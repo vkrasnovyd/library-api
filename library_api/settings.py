3b968444124571db81acfad2934c7fb6463f4945
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-03-11 19:20:47 +0100

    Add Throttling

--- library_api/settings.py
+++ library_api/settings.py
@@ -145,6 +145,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 SPECTACULAR_SETTINGS = {
